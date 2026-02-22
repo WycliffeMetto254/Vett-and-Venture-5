bcc73f4f48119dc2c78926b57a8f15fc4f9378c0
WycliffeMetto254 <wykiemetto@gmail.com>
2026-02-22 22:32:16 +0300

    index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Make sure you have this for Tailwind
 
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
