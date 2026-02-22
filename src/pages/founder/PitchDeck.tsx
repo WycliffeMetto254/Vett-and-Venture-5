c2c99106f166ac8449d88e9976b6ec287b036751
WycliffeMetto254 <wykiemetto@gmail.com>
2026-02-22 22:36:27 +0300

    PitchDeck.tsx

--- src/pages/founder/PitchDeck.tsx
+++ src/pages/founder/PitchDeck.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+import Layout from "../../components/Layout";
 
+const PitchDeck = () => {
+  return (
+    <Layout>
+      <h2 className="text-xl mb-4">Pitch Deck</h2>
+      <p>Here, founders can input and view their pitch deck details.</p>
+    </Layout>
+  );
+};
+
+export default PitchDeck;
