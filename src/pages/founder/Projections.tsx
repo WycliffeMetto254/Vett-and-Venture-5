a83df616d6fb084f017f8019ec9b6038165b5e91
WycliffeMetto254 <wykiemetto@gmail.com>
2026-02-22 22:36:53 +0300

    Projections.tsx

--- src/pages/founder/Projections.tsx
+++ src/pages/founder/Projections.tsx
@@ -1,1 +1,14 @@
 
+import React from "react";
+import Layout from "../../components/Layout";
+
+const Projections = () => {
+  return (
+    <Layout>
+      <h2 className="text-xl mb-4">Projections</h2>
+      <p>Here, founders can input and view financial projections.</p>
+    </Layout>
+  );
+};
+
+export default Projections;
