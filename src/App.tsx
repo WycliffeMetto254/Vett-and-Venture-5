bd75de21d5b9fd79541bb8ea5338d373096e9c9b
WycliffeMetto254 <wykiemetto@gmail.com>
2026-02-22 22:33:12 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import PitchDeck from "./pages/founder/PitchDeck";
+import Projections from "./pages/founder/Projections";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/pitchdeck" element={<PitchDeck />} />
+        <Route path="/projections" element={<Projections />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
